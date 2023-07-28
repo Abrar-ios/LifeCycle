8a5947fb4059ff910a38a074074774142ae61aca
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-07-28 23:29:52 +0500

    Practice to use Setup() & teardown()
    -Correct implementation to remove duplicate code
    - Duplicate code mode in setup()
    - use teardown() to remove instance on end of each test case
    - XCTest following steps in each test case
    -- setup()
    --run method
    -- teardown()
    Note: Don't overuse of setup(), try to limit setup, to things  that matter to most tests in suit

--- LifeCycleTests/MyClassTests.swift
+++ LifeCycleTests/MyClassTests.swift
@@ -9,7 +9,17 @@ import XCTest
 @testable import LifeCycle
 
 final class MyClassTest: XCTestCase {
-    private let sut = MyClass()
+    private var sut: MyClass!
+    
+    override func setUp() {
+        super.setUp()
+        sut = MyClass()
+    }
+    
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
     
     func test_mthodOne(){
         sut.methodOne()
