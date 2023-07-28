036af9a6459595abd1ca7527ad71d61cec787c5b
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-07-28 17:48:28 +0500

    Initial Commit

---
+++ LifeCycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LifeCycle
+//
+//  Created by HAQQQABD on 28/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
