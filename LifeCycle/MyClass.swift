458f3a535e6666eccfcda05724919944873f6213
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-07-28 18:00:37 +0500

    Create MyClass and MyClassTests
    To practice test suits to test MyClass methods logs without seeing console

---
+++ LifeCycle/MyClass.swift
@@ -0,0 +1,18 @@
+//
+//  MyClass.swift
+//  LifeCycle
+//
+//  Created by HAQQQABD on 28/07/2023.
+//
+
+import Foundation
+
+class MyClass {
+    func methodOne(){
+        print("methodOne")
+    }
+    
+    func methodTwo(){
+        print("methodTwo")
+    }
+}
---
+++ LifeCycleTests/MyClassTests.swift
@@ -0,0 +1,12 @@
+//
+//  MyClassTests.swift
+//  LifeCycleTests
+//
+//  Created by HAQQQABD on 28/07/2023.
+//
+
+import XCTest
+
+final class MyClassTest: XCTestCase {
+    
+}
